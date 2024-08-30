4dcc104cb29df1d67b7184e037ed705b4633c3b9
Nihil K'ri <NihilKri@live.com>
2024-08-30 14:02:27 -0500

    Section 2.5

--- MAT243Statistics.py
+++ MAT243Statistics.py
@@ -421,22 +421,34 @@ def section24test():
 
 
 def tscore(x:float, n:int, mean:float, std:float, r:int = 3):
-  t = (x - mean) / (std / math.sqrt(n))
-  return t
+  std = std / math.sqrt(n)
+  t = (x - mean) / std
+  return t, mean, std
 
 
 
 def section25test():
-  x, n, mean, std = 0, 1, 0, 1
-  t = tscore(x, n, mean, std)
+  x, n, mean, std, x2 = 2.72, 15, 1.86, 2.12, 3.26
+  t, mean1, std1 = tscore(x, n, mean, std)
+  t2, mean2, std = tscore(x2, n, mean, std)
 
   print(f"t = {t}, mean = {mean}, std = {std}")
   print("At most", x)
-  print(st.t.cdf(t, n, mean, std))
-  print(st.t.cdf(t, n, 0, 1))
+  print(st.t.cdf(x, n-1, mean, std))
+  print(st.t.cdf(t, n-1, 0, 1))
   print("At least", x)
-  print(st.t.sf(x, n, mean, std))
-  print(st.t.sf(t, n, 0, 1))
+  print(st.t.sf(x, n-1, mean, std))
+  print(st.t.sf(t, n-1, 0, 1))
+  print("Between", x, "and", x2)
+  print(st.t.cdf(x2, n-1, mean, std) - st.t.cdf(x, n-1, mean, std))
+  print(st.t.cdf(t2, n-1, 0, 1) - st.t.cdf(t, n-1, 0, 1))
+
+  # Find the 25th percentile of the t-distribution with 30 degrees of freedom. Type as #.###
+  print(st.t.ppf(0.25, 30, 0, 1))
+  # Find the 60th percentile of the t-distribution with 4 degrees of freedom. Type as #.###
+  print(st.t.ppf(0.60, 4, 0, 1))
+
+
 
   
 
