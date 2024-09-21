d346dc42fe139b5c7a8f90af6e2c140eff8a5a41
Nihil K'ri <NihilKri@live.com>
2024-09-21 17:22:37 -0500

    CA3.5.1: Hypothesis test for a population mean

--- Module3ConfidenceIntervals.py
+++ Module3ConfidenceIntervals.py
@@ -93,7 +93,7 @@ def GuaranteedSampleSize(c:float, std:float, marginOfError:float, pop:bool, p:fl
     n = (Tstar(c, df) * sampleStd / marginOfError) ** 2
   return n
 
-def HypTest(a:float, sampleMean:float, hypPopMean:float, std:float, n:int, pop:bool, tail:int):
+def HypTest(a:float, sampleMean:float, hypPopMean:float, std:float, n:int, pop:bool, tail:int, sig:int=3):
   """ Section 3.5.1 z-test for population means
       A z-test is a hypothesis test in which the z-statistic follows a normal distribution.
       The z-test for a population mean can be used to determine whether the population mean
@@ -107,137 +107,99 @@ def HypTest(a:float, sampleMean:float, hypPopMean:float, std:float, n:int, pop:b
 """
   if pop:
     popStd = std
-    z = (sampleMean - hypPopMean) / (popStd / n ** 0.5)
+    z = round((sampleMean - hypPopMean) / (popStd / n ** 0.5), sig)
+    print(f"z = {z:.3f}")
+    if tail == -1:
+      p = st.norm.cdf(z, 0, 1)
+    elif tail == 1:
+      p = st.norm.sf(z, 0, 1)
+    else:
+      p = st.norm.cdf(-z, 0, 1) + st.norm.sf(z, 0, 1)
   else:
     sampleStd = std
-    z = 0
-  print(f"z = {z:.3f}")
-  p = ((st.norm.cdf(z, 0, 1) if tail <= 0 else 0) +
-        (st.norm.sf(z, 0, 1) if tail >= 0 else 0))
+    t = round((sampleMean - hypPopMean) / (sampleStd / n ** 0.5), sig)
+    print(f"t = {t:.3f}")
+    print(f"df = {n - 1}")
+    if tail == -1:
+      p = st.t.cdf(t, n - 1, 0, 1) 
+    elif tail == 1:
+      p = st.t.sf(t, n - 1, 0, 1)
+    else:
+      p = st.t.cdf(-t, n - 1, 0, 1) + st.t.sf(t, n - 1, 0, 1)
   print(f"p = {p:.3f}")
-  print("Fails to reject H0" if p > a else "Rejects H0")
+  print("Fails to reject null hypothesis" if p > a else "Rejects null hypothesis")
 
 
 
-
-  pass
 
 def section3():
-  section = "PA3.5.1: Intelligence quotient"
+  section = "CA3.5.1: Hypothesis test for a population mean"
 
-  if section == "PA3.2.2: Confidence interval":
-    dat, popStd = [10, 17, 17.5, 18.5, 19.5], 1.25
-    n, sampleMean, sampleStd = MeanStd(dat, True)
-    print(f"Sample mean = {sampleMean:.3f}, Pop Std = {popStd:.3f}, n = {n}")
-    for c in [0.90, 0.99]:
-      print(f"Confidence Level = {c * 100}%")
-      standardError = popStd / (n ** 0.5)
-      print(f"Standard Error = {standardError:.3f}")
-      moe = MarginOfError(c, popStd, n, True)
-      print(f"Margin of Error = {moe:.3f}")
-      ci = ConfidenceInterval(c, sampleMean, popStd, n, True)
-      print(f"Confidence Interval = [{ci[0]:.3f}, {ci[1]:.3f}]")
-      # Python-Function 3.2.1: norm.interval()
-      ci = st.norm.interval(c, sampleMean, standardError)
-      print(f"ST Confidence Interval = [{ci[0]:.3f}, {ci[1]:.3f}]")
-      print()
+  if section == "":
+    print(f"{0:.3f}")
 
-  elif section == "Example 3.2.2: Microbeads in a water reservoir":
-    c, marginOfError, popStd = 0.95, 0.8, 2.5
-    print(f"{GuaranteedSampleSize(c, popStd, marginOfError, True):0.3f}")
+  elif section == "":
+    print(f"{0:.3f}")
 
-  elif section == "PA3.2.3: Tax assessment":
-    sampleMean, popStd = 1400, 1000
-    print(f"{math.ceil(GuaranteedSampleSize(0.90, 1000, 100, True))}")
 
-  elif section == "Example 3.2.3: Circumference of basketballs":
-    popMean, n, sampleMean, sampleStd, c = 29, 25, 29.1, 0.217, 0.99
-    ci = ConfidenceInterval(c, sampleMean, sampleStd, n, False)
-    print(FormatCI(ci))
+
+
+
+
+
+
+
+
+
+
+
+
   
-  elif section == "PA3.2.4: Weights of pumpkins":
-    dat = [5, 7, 7.5, 8, 8.5, 8.75]
-    n, sampleMean, sampleStd = MeanStd(dat, True)
-    df = n - 1
-    c = 0.90
-    cv = Tstar(c, df)
-    moe = MarginOfError(c, sampleStd, n, False)
-    ci = ConfidenceInterval(c, sampleMean, sampleStd, n, False)
-    print(f"1. df = {df}")
-    print(f"2. cv = {cv:.3f}")
-    print(f"3. moe = {moe:.3f}")
-    print(f"4. ci = {FormatCI(ci)}")
+  elif section == "CA3.5.1: Hypothesis test for a population mean":
+    q = 4
+    if q == 2:
+      HypTest(0.05, 10.06, 10.0, 0.24, 74, True, 1, 2)
+    elif q == 3:
+      HypTest(0.10, 13.09, 13.0, 0.26, 14, False, 0, 3)
+    elif q == 4:
+      HypTest(0.05, 20.05, 20.0, 0.25, 33, False, -1, 3)
 
-  elif section == "Python-Function 3.2.2: t.interval()":
-    n, sampleMean, sampleStd = 100, 219, 35.0
-    standardError = sampleStd / (n ** 0.5)
-    df = n - 1
-    c = 0.95
-    ci = st.t.interval(c, df, sampleMean, standardError)
-    print(f"{FormatCI(ci)}")
+  elif section == "Example 3.5.3: Circumference of basketballs":
+    hypPopMean = 29
+    n = 25
+    sampleMean = 29.1
+    sampleStd = 0.217
+    tail = 0
+    a = 0.01
+    HypTest(a, sampleMean, hypPopMean, sampleStd, n, False, tail)
   
-  elif section == "Example 3.2.4: Quality control":
-    moe = 0.35
-    c = 0.95
-    sampleStd = 2.50
-    print(f"{math.ceil(GuaranteedSampleSize(c, sampleStd, moe, False))}")
-
-  elif section == "PA3.2.5: Machine output":
-    moe = 2.1
-    c = 0.90
-    sampleStd = 7.3
-    print(f"{round(GuaranteedSampleSize(c, sampleStd, moe, True))}")
-    print(f"{round(GuaranteedSampleSize(c, sampleStd, moe, False))}")
- 
-  elif section == "CA3.2.1: Confidence intervals for population means":
-    dat = 74, 68, 64, 72, 68, 69, 67, 64, 62
-    popStd = 4
-    n, sampleMean, sampleStd = MeanStd(dat, True)
-    c = 0.90
-    print(f"x = {sampleMean:.3f}")
-    print(f"Moe = {MarginOfError(c, popStd, n, True):.3f}")
-    print(f"CI = {FormatCI(ConfidenceInterval(c, sampleMean, popStd, n, True))}")
-    print()
-
-    n, sampleMean, sampleStd = 11, 12, 3
-    c = 0.95
-    print(f"t* = {Tstar(c, n - 1):.3f}")
-    print(f"Moe = {MarginOfError(c, sampleStd, n, False):.3f}")
-    print(f"CI = {FormatCI(ConfidenceInterval(c, sampleMean, sampleStd, n, False))}")
-
-  elif section == "Example 3.3.1: Confidence interval for the proportion":
-    n = 1200
-    p = 348 / n
-    c = 0.95
-    print(f"CI = {FormatCI(ConfidenceInterval(c, p, 0, n, True))}")
+  elif section == "PA3.5.1: Intelligence quotient":
+    hypPopMean = 100
+    n = 36
+    sampleMean = 105
+    popStd = 15
+    a = 0.05
+    tail = 1
+    HypTest(a, sampleMean, hypPopMean, popStd, n, True, tail)
   
-  elif section == "PA3.3.1: Confidence interval for the proportion":
-    n = 1000
-    p = 281 / n
-    print(f"1. p = {p:.3f}")
-    ci = ConfidenceInterval(0.90, p, 0, n, True)
-    print(f"2. moe90 = {(ci[1] - ci[0])/2:.3f}")
-    ci = ConfidenceInterval(0.95, p, 0, n, True)
-    print(f"3. moe95 = {(ci[1] - ci[0])/2:.3f}")
-    print(f"4. CI95 = {FormatCI(ci)}")
-    # Python-Function 3.3.1: norm.interval()
-    stderr = (p * (1 - p)/n) ** 0.5
-    print(FormatCI(st.norm.interval(0.95, p, stderr)))
-
-  elif section == "Example 3.3.2: Six months after Brexit":
-    moe = 0.0217
-    c = 0.90
-    for a, p in [('a', 0.47), ('b', 0.50)]:
-      print(f"{a}. {math.ceil(GuaranteedSampleSize(c, 0, moe, True, p))}")
+  elif section == "Example 3.5.2: Carry-on baggage volume":
+    hypPopMean = 1.6
+    n = 73
+    sampleMean = 1.7
+    popStd = 0.29
+    a = 0.10
+    tail = 1
+    HypTest(a, sampleMean, hypPopMean, popStd, n, True, tail)
   
-  elif section == "PA3.3.2: Margin of error and sample size for proportions":
-    p = 0.36
-    moe = 0.01
-    print(f"1. {(GuaranteedSampleSize(0.90, 0, moe, True, p)):.3f}")
-    print(f"2. {(GuaranteedSampleSize(0.95, 0, moe, True, p)):.3f}")
-    print(f"3. {math.ceil(GuaranteedSampleSize(0.95, 0, moe, True, 0.50))}")
-    print(math.ceil(6234.682))
-
+  elif section == "Example 3.5.1: Mean battery life":
+    hypPopMean = 7.8
+    n = 10
+    sampleMean = 7.6
+    popStd = 0.57
+    a = 0.05
+    tail = -1
+    HypTest(a, sampleMean, hypPopMean, popStd, n, True, tail)
+  
   elif section == "CA3.3.1: Confidence intervals for population proportions":
     n = 1000
     p = 618 / n
@@ -259,33 +221,119 @@ def section3():
     c = 0.95
     print(GuaranteedSampleSize(c, 0, m, True, 0.5))
 
-  elif section == "Example 3.5.1: Mean battery life":
-    hypPopMean = 7.8
-    n = 10
-    sampleMean = 7.6
-    popStd = 0.57
-    a = 0.05
-    tail = -1
-    HypTest(a, sampleMean, hypPopMean, popStd, n, True, tail)
+  elif section == "PA3.3.2: Margin of error and sample size for proportions":
+    p = 0.36
+    moe = 0.01
+    print(f"1. {(GuaranteedSampleSize(0.90, 0, moe, True, p)):.3f}")
+    print(f"2. {(GuaranteedSampleSize(0.95, 0, moe, True, p)):.3f}")
+    print(f"3. {math.ceil(GuaranteedSampleSize(0.95, 0, moe, True, 0.50))}")
+    print(math.ceil(6234.682))
+
+  elif section == "Example 3.3.2: Six months after Brexit":
+    moe = 0.0217
+    c = 0.90
+    for a, p in [('a', 0.47), ('b', 0.50)]:
+      print(f"{a}. {math.ceil(GuaranteedSampleSize(c, 0, moe, True, p))}")
   
-  elif section == "Example 3.5.2: Carry-on baggage volume":
-    hypPopMean = 1.6
-    n = 73
-    sampleMean = 1.7
-    popStd = 0.29
-    a = 0.10
-    tail = 1
-    HypTest(a, sampleMean, hypPopMean, popStd, n, True, tail)
+  elif section == "PA3.3.1: Confidence interval for the proportion":
+    n = 1000
+    p = 281 / n
+    print(f"1. p = {p:.3f}")
+    ci = ConfidenceInterval(0.90, p, 0, n, True)
+    print(f"2. moe90 = {(ci[1] - ci[0])/2:.3f}")
+    ci = ConfidenceInterval(0.95, p, 0, n, True)
+    print(f"3. moe95 = {(ci[1] - ci[0])/2:.3f}")
+    print(f"4. CI95 = {FormatCI(ci)}")
+    # Python-Function 3.3.1: norm.interval()
+    stderr = (p * (1 - p)/n) ** 0.5
+    print(FormatCI(st.norm.interval(0.95, p, stderr)))
+
+  elif section == "Example 3.3.1: Confidence interval for the proportion":
+    n = 1200
+    p = 348 / n
+    c = 0.95
+    print(f"CI = {FormatCI(ConfidenceInterval(c, p, 0, n, True))}")
   
-  elif section == "PA3.5.1: Intelligence quotient":
-    hypPopMean = 100
-    n = 36
-    sampleMean = 105
-    popStd = 15
-    a = 0.05
-    tail = 1
-    HypTest(a, sampleMean, hypPopMean, popStd, n, True, tail)
+  elif section == "CA3.2.1: Confidence intervals for population means":
+    dat = 74, 68, 64, 72, 68, 69, 67, 64, 62
+    popStd = 4
+    n, sampleMean, sampleStd = MeanStd(dat, True)
+    c = 0.90
+    print(f"x = {sampleMean:.3f}")
+    print(f"Moe = {MarginOfError(c, popStd, n, True):.3f}")
+    print(f"CI = {FormatCI(ConfidenceInterval(c, sampleMean, popStd, n, True))}")
+    print()
+
+    n, sampleMean, sampleStd = 11, 12, 3
+    c = 0.95
+    print(f"t* = {Tstar(c, n - 1):.3f}")
+    print(f"Moe = {MarginOfError(c, sampleStd, n, False):.3f}")
+    print(f"CI = {FormatCI(ConfidenceInterval(c, sampleMean, sampleStd, n, False))}")
+
+  elif section == "PA3.2.5: Machine output":
+    moe = 2.1
+    c = 0.90
+    sampleStd = 7.3
+    print(f"{round(GuaranteedSampleSize(c, sampleStd, moe, True))}")
+    print(f"{round(GuaranteedSampleSize(c, sampleStd, moe, False))}")
+ 
+  elif section == "Example 3.2.4: Quality control":
+    moe = 0.35
+    c = 0.95
+    sampleStd = 2.50
+    print(f"{math.ceil(GuaranteedSampleSize(c, sampleStd, moe, False))}")
+
+  elif section == "Python-Function 3.2.2: t.interval()":
+    n, sampleMean, sampleStd = 100, 219, 35.0
+    standardError = sampleStd / (n ** 0.5)
+    df = n - 1
+    c = 0.95
+    ci = st.t.interval(c, df, sampleMean, standardError)
+    print(f"{FormatCI(ci)}")
   
+  elif section == "PA3.2.4: Weights of pumpkins":
+    dat = [5, 7, 7.5, 8, 8.5, 8.75]
+    n, sampleMean, sampleStd = MeanStd(dat, True)
+    df = n - 1
+    c = 0.90
+    cv = Tstar(c, df)
+    moe = MarginOfError(c, sampleStd, n, False)
+    ci = ConfidenceInterval(c, sampleMean, sampleStd, n, False)
+    print(f"1. df = {df}")
+    print(f"2. cv = {cv:.3f}")
+    print(f"3. moe = {moe:.3f}")
+    print(f"4. ci = {FormatCI(ci)}")
+
+  elif section == "Example 3.2.3: Circumference of basketballs":
+    popMean, n, sampleMean, sampleStd, c = 29, 25, 29.1, 0.217, 0.99
+    ci = ConfidenceInterval(c, sampleMean, sampleStd, n, False)
+    print(FormatCI(ci))
+  
+  elif section == "PA3.2.3: Tax assessment":
+    sampleMean, popStd = 1400, 1000
+    print(f"{math.ceil(GuaranteedSampleSize(0.90, 1000, 100, True))}")
+
+  elif section == "Example 3.2.2: Microbeads in a water reservoir":
+    c, marginOfError, popStd = 0.95, 0.8, 2.5
+    print(f"{GuaranteedSampleSize(c, popStd, marginOfError, True):0.3f}")
+
+  elif section == "PA3.2.2: Confidence interval":
+    dat, popStd = [10, 17, 17.5, 18.5, 19.5], 1.25
+    n, sampleMean, sampleStd = MeanStd(dat, True)
+    print(f"Sample mean = {sampleMean:.3f}, Pop Std = {popStd:.3f}, n = {n}")
+    for c in [0.90, 0.99]:
+      print(f"Confidence Level = {c * 100}%")
+      standardError = popStd / (n ** 0.5)
+      print(f"Standard Error = {standardError:.3f}")
+      moe = MarginOfError(c, popStd, n, True)
+      print(f"Margin of Error = {moe:.3f}")
+      ci = ConfidenceInterval(c, sampleMean, popStd, n, True)
+      print(f"Confidence Interval = [{ci[0]:.3f}, {ci[1]:.3f}]")
+      # Python-Function 3.2.1: norm.interval()
+      ci = st.norm.interval(c, sampleMean, standardError)
+      print(f"ST Confidence Interval = [{ci[0]:.3f}, {ci[1]:.3f}]")
+      print()
+
 
     
 
@@ -301,7 +349,4 @@ def section3():
 
 
 
-  elif section == "":
-    print(f"{0:.3f}")
-  
 
