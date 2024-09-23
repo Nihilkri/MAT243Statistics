ff4cf436c19e42fb72ddc0d866dd531792dc726e
Nihilkri <nehilkori@gmail.com>
2024-09-23 15:00:00 -0500

    zyDE 4.1.2: Unpaired t-test

--- Module4HypothesisTestTwo.py
+++ Module4HypothesisTestTwo.py
@@ -2,7 +2,7 @@
 import scipy.stats as st
 #import Calculus
 
-def Ztest2(sampleMean1:float, sampleMean2:float, std1:float, std2:float, n1:int, n2:int, tail:int, a:float, sig:int=3):
+def Ztest2(sampleMean1:float, sampleMean2:float, popStd1:float, popStd2:float, n1:int, n2:int, tail:int, a:float, sig:int=3):
   """ Section 4.1.1 Two-sample z-test for population means
       The z-test can also be used to determine whether the means of two independent populations are
       the same when the population standard deviations are known. When performing a hypothesis test
@@ -12,7 +12,6 @@ def Ztest2(sampleMean1:float, sampleMean2:float, std1:float, std2:float, n1:int,
       is needed.
       The conditions that must be satisfied are similar to those of a z-test for a population mean.
   """
-  popStd1, popStd2 = std1, std2
   stderr = round((popStd1 ** 2 / n1 + popStd2 ** 2 / n2) ** 0.5, sig)
   print(f"{stderr = :.6f}")
   z = round((sampleMean1 - sampleMean2 - 0) / stderr, sig)
@@ -31,7 +30,6 @@ def pairedTTest(sampleMeanDifference:float, hypMeanDifference:float, sampleStd:f
        In a paired t-test or dependent t-test, a sample taken from one population is exposed to two different treatments. The main idea is that measurements are recorded from the same group, usually before and after a treatment is applied or when each of two treatments is applied. Ex: A group of professional cycling athletes is selected for a study on the effects of caffeine dosage on exhaustion times. The populations are the cyclists for each of two dosages. The samples are the measured exhaustion times for each dosage, which implies dependence because the measurements were taken from the same group.
   """
   t = round((sampleMeanDifference - hypMeanDifference) / (sampleStd / n ** 0.5), sig)
-  t = 1.418
   print(f"{t = :.6f}")
   df = n - 1
   if tail == -1:
@@ -44,17 +42,30 @@ def pairedTTest(sampleMeanDifference:float, hypMeanDifference:float, sampleStd:f
   print("Fails to reject null hypothesis" if p > a else "Rejects null hypothesis")
   
   
-def UnpairedTTest():
+def UnpairedTTest(sampleMean1:float, sampleMean2:float, hypSampleMean1:float, hypSampleMean2:float, sampleStd1:float, sampleStd2:float, n1:int, n2:int, tail:int, a:float, sig:int=3):
   """ Section 4.1.2 Two-sample t-test
        In an unpaired t-test or independent t-test, a sample taken from one population is not related to a different sample taken from another population. In contrast to the paired t-test, measurements from an unpaired t-test are recorded from different groups when exposed to the same treatment. Ex: The effect of caffeine intake on exhaustion times is studied by measuring the exhaustion times of a randomly selected group of 9 professional cyclists taking caffeine pills and another group of 9 cyclists not taking caffeine pills. The two populations are all cyclists taking caffeine pills and those who are not taking the pills. The samples are the measured exhaustion times from the two groups, each with 9 cyclists, which implies independence because the times are for two different groups of cyclists.
   """
-  pass
+  stderr = round((sampleStd1 ** 2 / n1 + sampleStd2 ** 2 / n2) ** 0.5, sig)
+  print(f"{stderr = :.6f}")
+  t = round((sampleMean1 - sampleMean2 - (hypSampleMean1 - hypSampleMean2)) / stderr, sig)
+  print(f"{t = :.6f}")
+  df = n1 + n2 - 2
+  if tail == -1:
+    p = st.t.cdf(t, df, 0, 1)
+  elif tail == 1:
+    p = st.t.sf(t, df, 0, 1)
+  else:
+    p = st.t.cdf(-abs(t), df, 0, 1) + st.t.sf(abs(t), df, 0, 1)
+  print(f"{p = :.6f}")
+  print("Fails to reject null hypothesis" if p > a else "Rejects null hypothesis")
+
         
         
 
 
 def section4():
-  section = "zyDE 4.1.1: Paired t-test"
+  section = "zyDE 4.1.2: Unpaired t-test"
 
   if section == "":
     print(f"{0:.3f}")
@@ -62,9 +73,20 @@ def section4():
   elif section == "":
     print(f"{0:.3f}")
 
-  elif section == "Example 4.1.3: Improvement in exam scores":
-    print(f"{0:.3f}")
+  elif section == "zyDE 4.1.2: Unpaired t-test":
+    """ The st.ttest_ind(x, y) command takes two arrays or DataFrame columns x and y as inputs and returns the t-statistic and the corresponding two-tailed p-value as outputs.
 
+The code below conducts the test for the packing machine example above. Both the two-tailed test and the one-tailed test are conducted.
+"""
+    import pandas as pd
+    df = pd.read_csv('Machine.csv')
+    
+    # Two-tailed test
+    print(st.ttest_ind(df['Old'],df['New'],equal_var=False, alternative="two-sided"))
+    
+    # One-tailed test
+    print(st.ttest_ind(df['Old'],df['New'],equal_var=False, alternative="greater"))
+    
   elif section == "zyDE 4.1.1: Paired t-test":
     """ The st.ttest_rel(x, y) function takes two arrays or DataFrame columns x and y with the same length as inputs and returns the t-statistic and the corresponding two-tailed p-value as outputs.
 
