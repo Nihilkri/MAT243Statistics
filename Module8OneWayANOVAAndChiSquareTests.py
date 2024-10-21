901b144e8dd31e05d25e9905d0ff4391a4c07b69
Nihil K'ri <NihilKri@live.com>
2024-10-21 00:43:49 -0500

    PA8.2.2: Calculating expected cell counts

--- Module8OneWayANOVAAndChiSquareTests.py
+++ Module8OneWayANOVAAndChiSquareTests.py
@@ -1,6 +1,24 @@
+from operator import index
 from Basic import *
 
 def Chi2(dat: pd.DataFrame, proportion: bool) -> None:
+  """ Section 8.2.1 Chi-square goodness-of-fit test
+        The chi-square distribution can be used to test how close the distribution of a population
+        is to a theoretical distribution. The chi-squared test statistic measures how different
+        the observed counts are compared to the expected counts, assuming the null hypothesis is
+        true. Specifically, Chi**2 = sum((O[i] - E[i]) ** 2 / E[i]).
+        
+        The difference in observed and expected counts is squared to account for the fact that
+        some of the absolute differences will be positive and some will be negative. Squaring the
+        differences ensures that all terms will be positive. Dividing by the expected count is
+        necessary to scale the differences as a percentage. Ex: If the expected count is 5, a
+        squared difference of 10 is 200% of the expected count (10/5 = 2). A squared difference
+        of 10 is much less relative to an expected count of 500 (10/500 = 0.02).
+        
+        The alternate hypothesis for a chi-squared test is always two-sided, meaning that the
+        proportions are not equal. Thus, if the null hypothesis is rejected, no conclusion can be
+        made on which proportion is larger.
+  """
   if proportion:
     t = dat["Observed"].sum()
     dat["Expected"] *= t
@@ -28,10 +46,45 @@ def Chi2(dat: pd.DataFrame, proportion: bool) -> None:
   print(f"{statistic = :.4f}")
   print(f"{pvalue = :.4f}")
 
+def ContingencyTable(obs:pd.DataFrame, hasTotals:bool=False) -> None:
+  """ Section 8.2.2
+        A chi-square test can also be used to determine whether two or more variables are
+        independent by comparing the distributions of the variables over two or more categories.
+        This test is the chi-square test for independence. The expected counts used to calculate
+        the test statistic in this case come from a contingency table. A contingency table is
+        constructed from the values of the variables and categories along the rows and columns. An
+        expected cell count is calculated by multiplying the row total by the column total and
+        dividing by the overall total.
+  """
+  dct = {}
+  if hasTotals:
+    tx = obs.columns[-1]
+    ty = obs.index[-1]
+  else:
+    pass
+  for x in obs.columns:
+    lst = []
+    for y in (obs.index[:-1] if hasTotals else obs.index):
+      lst.append(obs[tx][y] * (1 if (hasTotals and x == tx) else obs[x][ty] / obs[tx][ty]))
+    if hasTotals:
+      lst.append(obs[x][ty])
+    dct[x] = lst
+  exp = pd.DataFrame(dct, index=obs.index)
+  print("Observed")
+  print(obs)
+  print()
+  print("Expected")
+  print(exp)
+
+def Chi2Test(dat:pd.DataFrame) -> None:
+
+
+  pass
+
 #==================================================================================================
 
 def Section8():
-  section = "CA8.2.1: Chi-square goodness of fit test"
+  section = "PA8.2.2: Calculating expected cell counts"
 
   if section == "":
     print(f"{0:.3f}")
@@ -45,11 +98,26 @@ def Section8():
   elif section == "":
     print(f"{0:.3f}")
     
-  elif section == "":
-    print(f"{0:.3f}")
+  elif section == "PA8.2.2: Calculating expected cell counts":
+    from io import StringIO
+    csv = StringIO(
+      ",Died,Survived,Total by specialty\n"
+      "Infantry,374,2629,3003\n"
+      "Non-infantry,73,1219,1292\n"
+      "Total by outcome,447,3848,4295")
+    dat = pd.read_csv(csv, index_col=0)
+    ContingencyTable(dat, True)
     
-  elif section == "":
-    print(f"{0:.3f}")
+  elif section == "Example 8.2.2: Calculating the expected counts for a 3x2 contingency table":
+    from io import StringIO
+    csv = StringIO(
+      "Punishment,Parole violation,No parole violation,Total by punishment\n"
+      "0,405,1422,1827\n"
+      "1-2,240,470,710\n"
+      "3 or greater,151,275,426\n"
+      "Total by violation,796,2167,2963")
+    dat = pd.read_csv(csv, index_col=0)
+    ContingencyTable(dat, True)  
     
   elif section == "CA8.2.1: Chi-square goodness of fit test":
     q = 3
